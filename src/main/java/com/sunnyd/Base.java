4efc0c54591a8243ce82e385ea0c1360bb3b4207
NimonikMike <viet_mike.pham@hotmail.com>
2013-10-02 22:37:33 -0400

    commet and refactor

--- src/main/java/com/sunnyd/Base.java
+++ src/main/java/com/sunnyd/Base.java
@@ -12,76 +12,82 @@ import java.util.HashMap;
 public class Base
 {
 
-  public Base(HashMap<Object, Object> HM)
-  {
-    String className = this.getClass().getName();
-
-    //GetClass
-//		Class.forName(className)
-
-    try
+    public Base(HashMap<Object, Object> HM)
     {
-      Field[] fields = Class.forName(className).getDeclaredFields();
+        //Get Caller ClassName
+        String className = this.getClass().getName();
 
-      for (Field field : fields)
-      {
-        String fieldName = field.getName();
-        Object value = HM.get(fieldName);
+        //Instantiate Current Class
+        Class<?> classObject = null;
+        try {
+            classObject = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        String capitalizeField = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+        //Get all object fields
+        Field[] fields = classObject.getDeclaredFields();
+        for (Field field : fields)
+        {
+            String fieldName = field.getName();
+            Object value = HM.get(fieldName);
+            String capitalizeField = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
 
-        java.lang.reflect.Method method;
+            java.lang.reflect.Method method;
+            try
+            {
+                try
+                {
+                    /*Expect all DB attribute to have mutator methods and is named
+                        getAttributeName
+                        setAttributeName
+                        ex: attribute id
+                            getId()
+                            setId()
+                     */
+                    method = classObject.getDeclaredMethod("set" + capitalizeField, field.getType());
+                }
+                catch (NoSuchMethodException e)
+                {
+                    break;// If method does not have setMethod then it is not a db Attribute
+                }
+
+                //2nd Verication: Verify method belong to a dbAttribute using annotaitons
+                //TODO verify SOLUTION 1: all getter Setter method = setDBFirstName or use annotations
+                Annotation ARMethod = method.getAnnotation(Method.class);
+
+                method.invoke(this, value);
+            }
+            catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T find(int id)
+    {
+        HashMap<Object, Object> HM = new Manager().find(id);
+        if (HM == null)
+        {
+            return null;
+        }
+
+        //Since this is a static method, to get caller of method we must look in the stack trace to get class
+        StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+        String className = ste[ste.length - 1].getClassName();
         try
         {
-          try
-          {
-            method = Class.forName(className).getDeclaredMethod("set" + capitalizeField, field.getType());
-          }
-          catch (NoSuchMethodException e)
-          {
-            break;// if method is not a setter or a Getter
-          }
-
-          //TODO verify SOLUTION 1: all getter Setter method = setDBFirstName or use annotations
-          Annotation ARMethod = method.getAnnotation(Method.class);
-
-          method.invoke(this, value);
-
+            return (T) Class.forName(className).getConstructor(HashMap.class).newInstance(HM);
         }
-        catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e)
         {
-          e.printStackTrace();
+            e.printStackTrace();
         }
-      }
-    }
-    catch (SecurityException | ClassNotFoundException e)
-    {
-      e.printStackTrace();
-    }
-  }
 
-  @SuppressWarnings("unchecked")
-  public static <T> T find(int id)
-  {
-    HashMap<Object, Object> HM = new Manager().find(id);
-    if (HM == null)
-    {
-      return null;
+        return null;
     }
-
-    StackTraceElement[] ste = Thread.currentThread().getStackTrace();
-    String className = ste[ste.length - 1].getClassName();
-    try
-    {
-      return (T) Class.forName(className).getConstructor(HashMap.class).newInstance(HM);
-    }
-    catch (ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e)
-    {
-      e.printStackTrace();
-    }
-
-    return null;
-  }
 
 
 }
