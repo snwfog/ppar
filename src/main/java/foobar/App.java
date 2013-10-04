a5425018b4aeccb0dcb730968b3339ed65ae22f5
Wais Khedri <khedriwais@hotmail.com>
2013-10-04 14:23:30 -0400

    refactor

--- src/main/java/foobar/App.java
+++ src/main/java/foobar/App.java
@@ -2,26 +2,20 @@ package foobar;
 
 import com.googlecode.flyway.core.Flyway;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       // System.out.println( "Hello World!" );
-       
-	       // Create the Flyway instance
-	       Flyway flyway = new Flyway();
+public class App {
+	public static void main(String[] args) {
+		// System.out.println( "Hello World!" );
 
-	       // Point it to the database
-	       //flyway.setDataSource("jdbc:h2:file:target/foobar", "sa", null);
-	       flyway.setDataSource("jdbc:mysql://localhost:3306/flyway", "root", null);
+		// Create the Flyway instance
+		Flyway flyway = new Flyway();
 
-	       // Start the migration
-	       flyway.migrate();
-	    
-    }
-    
+		// Point it to the database
+		// flyway.setDataSource("jdbc:h2:file:target/foobar", "sa", null);
+		flyway.setDataSource("jdbc:mysql://localhost:3306/flyway", "root", null);
+
+		// Start the migration
+		flyway.migrate();
+
+	}
+
 }
