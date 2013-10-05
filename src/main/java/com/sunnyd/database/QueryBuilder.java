f8b8d135074fb3d236f48bf4b0db1ba284a46d9c
snwfog <donchoa@gmail.com>
2013-10-05 17:13:41 -0400

    Remove some useless commentary in the Manager and QueryBuilder

--- src/main/java/com/sunnyd/database/QueryBuilder.java
+++ src/main/java/com/sunnyd/database/QueryBuilder.java
@@ -1,12 +1,5 @@
 package com.sunnyd.database;
 
-/**
- * Created with IntelliJ IDEA.
- * User: snw
- * Date: 2013-10-04
- * Time: 8:52 PM
- * To change this template use File | Settings | File Templates.
- */
 public class QueryBuilder
 {
 }
