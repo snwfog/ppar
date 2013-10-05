5aee1fa652083528516d7f25ab582ebc7bbbe8b0
Harry Zhang <haiyuezhang@hotmail.com>
2013-10-05 19:08:05 -0400

    type converter from java to sql

--- src/main/java/com/sunnyd/database/Manager.java
+++ src/main/java/com/sunnyd/database/Manager.java
@@ -8,7 +8,10 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Manager
@@ -21,17 +24,21 @@ public class Manager
 
         // sample test for CRUD below:
         HashMap<String, Object> map = new HashMap<String, Object>();
-        map.put("firstName", "save");
-        map.put("lastName", "asd");
+        
+        map.put("firstName", new Date());
+        map.put("asd", true);
+        map.put("lastName", 99.88);
 
         // FIND:
         // System.out.println(find(1, "persons"));
-        ArrayList<HashMap<String, Object>> r = findAll("persons", map);
+        
+        // FIND ALL:
+        /**ArrayList<HashMap<String, Object>> r = findAll("persons", map);
         for (HashMap<String, Object> h : r){
             for (String key : h.keySet()){
                 System.out.println(key + ":" + h.get(key));
             }
-        }
+        }*/
         
 
         // SAVE:
@@ -42,6 +49,20 @@ public class Manager
 
         // UPDATE:
         // System.out.println(update(3, "persons", map));
+        
+        
+        
+        
+        // converter Java to SQL:
+
+        
+        
+        HashMap<String,String> c = convertJavaSQL(map);
+        for (Object key : c.keySet())
+        {    
+            System.out.println(key + " " + c.get(key));
+            
+        }
 
     }
 
@@ -246,6 +267,42 @@ public class Manager
     public static void create()
     {
         // create obj
+    }
+    
+    
+    // helper method to convert java type (string, int, date) to mysql type written in str
+    private static HashMap<String, String> convertJavaSQL(HashMap<String, Object> original){
+        HashMap<String, String> converted = new HashMap<String, String>();
+        
+        for (String key : original.keySet())
+        {
+            Class<?> fieldType = original.get(key).getClass();
+            String type = fieldType.getSimpleName();
+            
+            switch (type){
+                case "Boolean":
+                    converted.put(key, "'" + original.get(key).toString() + "'");
+                    break;
+                case "Integer":
+                    converted.put(key, Integer.toString((int) original.get(key)));
+                    break;
+                case "Double":
+                    converted.put(key, Double.toString((double) original.get(key)));
+                    break;
+                case "String":
+                    converted.put(key, "'" + original.get(key) + "'");
+                    break;
+                case "Date":
+                    Date dt = (Date) original.get(key);   
+                    DateFormat parser = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+                    converted.put(key, "'" + parser.format(dt) +"'");
+                    break;
+                default:
+                    System.out.println("i dunno this type yet, tell harry");
+                    break;
+            }
+        }
+        return converted;
     }
 
 }
