336de8a9cbf20110abb27c1de083af0bc1bde211
NimonikMike <viet_mike.pham@hotmail.com>
2013-10-05 16:40:00 -0400

    refactor and almost done save method in base

--- src/main/java/com/sunnyd/models/Person.java
+++ src/main/java/com/sunnyd/models/Person.java
@@ -7,67 +7,76 @@ import com.sunnyd.annotations.*;
 import java.util.HashMap;
 
 public class Person extends Base implements ModelInterface {
-	public static final String tableName = "persons";
-	
-	@tableAttr
-	private String firstName;
-	
-	@tableAttr
-	private String lastName;
-	
-	private String status;
+    public static final String tableName = "persons";
 
-	public Person(HashMap<String, Object> HM) {
-		super(HM);
-	}
+    @tableAttr
+    private String firstName;
 
-	public static void main(String[] args) {
-		
-		Person a = Person.find(1);
-		
-		System.out.println(a.getId());
-		System.out.println(a.getFirstName());
-		System.out.println(a.getLastName());
-		a.setFirstName("john");
+    @tableAttr
+    private String lastName;
 
-		
-		Person b = Person.find(2);
-		
-		System.out.println(b.getId());
-		System.out.println(b.getFirstName());
-		System.out.println(b.getLastName());
-		b.setFirstName("oaisjdoaijdoa");
+    private String status;
 
-		b.update();
-		a.update();
-//		System.out.println(a.Destroy());
-		
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
+    public Person(){
+        super();
+    }
+    public Person(HashMap<String, Object> HM) {
+        super(HM);
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-		setUpdateFlag(true);
-	}
+    public static void main(String[] args) {
 
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-		setUpdateFlag(true);
-	}
+//        Person a = new Person();
+//        System.out.println(a.getId());
+//        a.setFirstName("john");
+//        System.out.println(a.getFirstName());
+//        System.out.println(a.getLastName()); 
+//        a.save();
+          
+        Person a = Person.find(1);
 
-	public String getStatus() {
-		return status;
-	}
+        System.out.println(a.getId());
+        System.out.println(a.getFirstName());
+        System.out.println(a.getLastName());
+        a.setFirstName("john");
+//
+//        Person b = Person.find(2);
+//
+//        System.out.println(b.getId());
+//        System.out.println(b.getFirstName());
+//        System.out.println(b.getLastName());
+//        b.setFirstName("oaisjdoaijdoa");
+//
+//        b.update();
+//        a.update();
+        // System.out.println(a.Destroy());
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        setUpdateFlag(true);
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        setUpdateFlag(true);
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
 }
