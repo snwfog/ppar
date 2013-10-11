876bc6f0d26081e376909b6e9d535acf53b05860
Wais Khedri <khedriwais@hotmail.com>
2013-10-11 19:35:52 -0400

    peer model

---
+++ src/main/java/com/sunnyd/models/Peer.java
@@ -0,0 +1,108 @@
+package com.sunnyd.models;
+
+import com.sunnyd.Base;
+import com.sunnyd.IModel;
+import com.sunnyd.annotations.*;
+
+import java.util.HashMap;
+
+public class Peer extends Base implements IModel {
+    public static final String tableName = "peers";
+
+    @tableAttr
+    private String firstName;
+
+    @tableAttr
+    private String lastName;
+
+    @tableAttr
+    private String email;
+
+    @tableAttr
+    private String userName;
+
+    @tableAttr
+    private String password;
+
+    @tableAttr
+    private Integer point;
+
+    @tableAttr
+    private Integer rankId;
+
+    @tableAttr
+    private String personalWebsite;
+
+    public Peer() {
+        super();
+    }
+
+    public Peer(HashMap<String, Object> HM) {
+        super(HM);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+
+    public Integer getRankId() {
+        return rankId;
+    }
+
+    public void setRankId(Integer rankId) {
+        this.rankId = rankId;
+    }
+
+    public String getPersonalWebsite() {
+        return personalWebsite;
+    }
+
+    public void setPersonalWebsite(String personalWebsite) {
+        this.personalWebsite = personalWebsite;
+    }
+
+}
