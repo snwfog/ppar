94dbbed46a84c0bb71c358bc660bf75e324e639e
Wais Khedri <khedriwais@hotmail.com>
2013-10-11 13:05:28 -0400

    refact

---
+++ src/main/java/com/sunnyd/flyway/migration/Migration.java
@@ -0,0 +1,20 @@
+package com.sunnyd.flyway.migration;
+
+import com.googlecode.flyway.core.Flyway;
+
+public class Migration {
+    public static void main(String[] args) {
+
+        // Create the Flyway instance
+        Flyway flyway = new Flyway();
+
+        // Point it to the database
+        // flyway.setDataSource("jdbc:h2:file:target/foobar", "sa", null);
+        flyway.setDataSource("jdbc:mysql://localhost:3306/ppardb", "root", null);
+
+        // Start the migration
+        flyway.migrate();
+
+    }
+
+}
