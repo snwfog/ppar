2d8713cb4f30d9cde4b10712800cef1747452ab2
snwfog <donchoa@gmail.com>
2013-11-10 19:24:29 -0500

    Revert the password

--- src/test/java/com/sunnyd/database/ManagerTest.java
+++ src/test/java/com/sunnyd/database/ManagerTest.java
@@ -38,7 +38,7 @@ public class ManagerTest extends DatabaseTestSetup
     p.setLastName("Yang");
     p.setEmail("donchoa@gmail.com");
     p.setUserName("snwfog");
-    p.setPassword("4321");
+    p.setPassword("1234");
     p.setPoint(9000);
     p.setPersonalWebsite("http://charlescy.com");
 
