7f8e054a5533f7b1b166ff8a2ae6a7dd692d65e4
Wais Khedri <khedriwais@hotmail.com>
2013-11-15 23:18:52 -0500

    modified peers table

--- src/main/resources/db/migration/V20131115.20.48.00__alter_table_peers.sql
+++ src/main/resources/db/migration/V20131115.20.48.00__alter_table_peers.sql
@@ -2,3 +2,4 @@ alter table peers add date_of_birth date;
 alter table peers add country varchar(256);
 alter table peers add industry varchar(256);
 alter table peers add experience int;
+alter table peers add gender varchar(256);
