f8ca33f9f79ffa3b26d757b97045724101c48913
snwfog <donchoa@gmail.com>
2013-12-09 16:19:52 -0500

    Modify signature for the Update method

--- src/main/java/com/sunnyd/Base.java
+++ src/main/java/com/sunnyd/Base.java
@@ -126,7 +126,7 @@ public class Base implements IModel
     return null;
   }
 
-  private static <T extends Base> boolean update(Class<T> classObject, Object instanceObject)
+  private static <T extends Base> boolean update(Class<T> classObject, T instanceObject)
   {
     Map<String, Object> updateAttributes = BaseHelper.getTableFieldNameAndValue(classObject, instanceObject);
     if (classObject.getAnnotation(ActiveRecordInheritFrom.class) != null)
@@ -146,7 +146,7 @@ public class Base implements IModel
   }
 
   @SuppressWarnings("unchecked")
-  private static <T extends Base> boolean updateRelation(Class<T> classObject, Object instanceObject)
+  private static <T extends Base> boolean updateRelation(Class<T> classObject, T instanceObject)
   {
     Field[] fields = classObject.getDeclaredFields();
     for (Field field : fields)
