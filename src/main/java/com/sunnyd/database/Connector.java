f1eef0a37984ba8819f4da817bfeeca76732ba5b
snwfog <donchoa@gmail.com>
2014-01-10 14:47:05 -0500

    Exclude sql files for package

--- src/main/java/com/sunnyd/database/Connector.java
+++ src/main/java/com/sunnyd/database/Connector.java
@@ -1,69 +1,66 @@
 package com.sunnyd.database;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class Connector
-{
-  static final Logger logger = LoggerFactory.getLogger(Connector.class);
-  final static String driverClass = "com.mysql.jdbc.Driver";
-  private static String database = "soen387::a2";
-  private static String host = "127.0.0.1";
-  private static String port = "3306";
-  private static String username = "root";
-  private static String password = "root";
-  private static String url;
-  //static reference to itself
-  private static Connector instance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  //private constructor
-  private Connector()
-  {
-    // Initiate the URL from properties files or fall back to default
-    Properties prop = new Properties();
-    try
-    {
-      // Load the files from the default path
-      // Which is located at resources/config
-      prop.load(this.getClass().getResourceAsStream("/config/database.properties"));
+public class Connector {
 
+    static final Logger logger = LoggerFactory.getLogger( Connector.class );
+    final static String driverClass = "com.mysql.jdbc.Driver";
+    private static String database = "soen387::a2";
+    private static String host = "127.0.0.1";
+    private static String port = "3306";
+    private static String username = "root";
+    private static String password = "root";
+    private static String url;
+    //static reference to itself
+    private static Connector instance;
 
-      // Statically init the JDBC class
-      Class.forName(driverClass);
-
-      // Set the URL string for the JDBC connection
-      database = prop.getProperty("database", database);
-      host = prop.getProperty("host", host);
-      port = prop.getProperty("port", port);
-      username = prop.getProperty("username", username);
-      password = prop.getProperty("password", password);
-      url = String.format("jdbc:mysql://%s:%s/%s", host, port, database);
+    public static void main( String[] args ) throws Throwable {
+        Connector.getConnection();
     }
-    catch (ClassNotFoundException e)
-    {
-      logger.error("Could not load driver class");
-    }
-    catch (IOException e)
-    {
-      logger.error("Could not load the database.properties file");
-    }
-  }
 
-  public static Connection getConnection() throws SQLException
-  {
-    if (instance == null)
-      instance = new Connector();
-    return instance.createConnection();
-  }
+    //private constructor
+    private Connector() {
+        // Initiate the URL from properties files or fall back to default
+        Properties prop = new Properties();
+        try {
+            // Load the files from the default path
+            // Which is located at resources/config
+            prop.load( this.getClass().getResourceAsStream( "/config/database.properties" ) );
 
-  private Connection createConnection() throws SQLException
-  {
-    return DriverManager.getConnection(url, username, password);
-  }
+
+            // Statically init the JDBC class
+            Class.forName( driverClass );
+
+            // Set the URL string for the JDBC connection
+            database = prop.getProperty( "database", database );
+            host = prop.getProperty( "host", host );
+            port = prop.getProperty( "port", port );
+            username = prop.getProperty( "username", username );
+            password = prop.getProperty( "password", password );
+            url = String.format( "jdbc:mysql://%s:%s/%s", host, port, database );
+        } catch ( ClassNotFoundException e ) {
+            logger.error( "Could not load driver class" );
+        } catch ( IOException e ) {
+            logger.error( "Could not load the database.properties file" );
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if ( instance == null ) {
+            instance = new Connector();
+        }
+        return instance.createConnection();
+    }
+
+    private Connection createConnection() throws SQLException {
+        return DriverManager.getConnection( url, username, password );
+    }
 }
